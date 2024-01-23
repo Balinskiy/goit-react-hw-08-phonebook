e7921d996c73f30d4dbd1406e982d2ebdbbb6f5a
Balinskiy <3879342@gmail.com>
2024-01-23 16:42:57 +0200

    install

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,6 +8,7 @@ import { getContacts } from 'store/selectors';
 import { fetchContacts } from 'store/operations';
 import Loader from 'components/Loader/Loader';
 import { Heading } from 'components/Heading/Heading.styled';
+import Navigation from 'components/Navigation/Navigation';
 
 const App = () => {
   const contacts = useSelector(getContacts);
@@ -20,6 +21,9 @@ const App = () => {
 
   return (
     <>
+      <header>
+        <Navigation />
+      </header>
       <Box>
         {isLoading && <Loader />}
         {error && <p>{error}</p>}
@@ -41,3 +45,47 @@ const App = () => {
 };
 
 export default App;
+
+// import React, { useEffect } from 'react';
+// import { Box } from './App.styled';
+// import ContactFormComponent from 'components/ContactForm/ContactForm';
+// import FilterComponent from 'components/Filter/Filter';
+// import ContactListComponent from 'components/ContactList/ContactList';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { getContacts } from 'store/selectors';
+// import { fetchContacts } from 'store/operations';
+// import Loader from 'components/Loader/Loader';
+// import { Heading } from 'components/Heading/Heading.styled';
+
+// const App = () => {
+//   const contacts = useSelector(getContacts);
+//   const { error, isLoading } = useSelector(state => state.contacts);
+
+//   const dispatch = useDispatch();
+//   useEffect(() => {
+//     dispatch(fetchContacts());
+//   }, [dispatch]);
+
+//   return (
+//     <>
+//       <Box>
+//         {isLoading && <Loader />}
+//         {error && <p>{error}</p>}
+//         <Heading as="h1" styletype="primary">
+//           Phonebook
+//         </Heading>
+//         <ContactFormComponent />
+//         <Heading as="h2" styletype="secondary">
+//           Contacts
+//         </Heading>
+//         <Heading as="h3" styletype="third">
+//           Find contact name by number:
+//         </Heading>
+//         <FilterComponent />
+//         {contacts.length > 0 && <ContactListComponent />}
+//       </Box>
+//     </>
+//   );
+// };
+
+// export default App;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <Navigation>
+      <NavLink to="/">Home</NavLink>
+    </Navigation>
+  );
+};
+
+export default Navigation;
